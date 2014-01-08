1d2cc3a8a5bbcb83ee58b2b258d3f4ea54ef888a
Stefan Thomas <justmoon@members.fsf.org>
2014-01-07 18:15:20 -0800

    Add UDID sanitization to /delete as well.

--- app.js
+++ app.js
@@ -217,6 +217,11 @@ app.post('/subscriptions/apn/delete', function(req, res) {
     throw new Error("No UDID provided!");
   }
   var udid = req.body.udid;
+  udid = udid.replace(/\s/g, '').replace(/^<|>$/g, '');
+
+  if (!/^[a-f0-9]{64}$/i.exec(udid)) {
+    throw new Error("Invalid UDID, should be 64 characters hex!");
+  }
 
   var endpoint = req.query.sandbox ? "apnd" : "apn";
 
